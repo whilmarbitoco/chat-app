580d799d31c5971473f824eaac5ac57eeb79c77e
Whilmar Bitoco <117832470+whilmarbitoco@users.noreply.github.com>
2023-02-01 13:11:38 +0000

    Modified the design

--- js/index.js
+++ js/index.js
@@ -1,15 +1,47 @@
 const app = Vue.createApp({
     data() {
         return {
+            chatData: [
+                {
+                    id: 18245,
+                    name: "whil",
+                    msg: "Your chat",
+                },
+                {
+                    id: 1832,
+                    name: "auser",
+                    msg: "Received chat",
+                },
+            ],
             chats: [],
+            receive: [],
+            currentUser: "whil",
             state: false,
         };
     },
     methods: {
         sendMessage(e) {
             e.preventDefault();
-            this.chats = [...this.chats, this.chats];
-            this.state = true;
+            if (this.chats.msg) {
+                this.chats.id = Math.floor(Math.random() * 194727);
+                this.chats.name = "whil";
+                this.chats = [...this.chats, this.chats];
+                this.state = true;
+                //console.log(this.chats);
+            }
         },
+    },
+    created() {
+        // use an api and replace the chatData with the actual chat datas from the api ==\/
+        //axios.get().then((chatData) => {});
+
+        const chat = this.chatData.filter(
+            (data) => data.name === this.currentUser
+        );
+        const rec = this.chatData.filter(
+            (data) => data.name !== this.currentUser
+        );
+        this.receive = [...rec];
+        this.chats = [...chat];
     },
 }).mount("#app");
